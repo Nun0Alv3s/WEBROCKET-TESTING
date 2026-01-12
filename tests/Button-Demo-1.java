c3e1e5a73b2832bc2e807d9f02b8b8a84ed84b28
Nuno Alves <nuno.alves@aptoide.com>
2026-01-12 15:43:54 +0000

    feat(test): integrate page object

---
+++ tests/Button-Demo-1.java
@@ -0,0 +1,26 @@
+package tests;
+
+import java.time.Duration;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Button_Demo_1 {
+
+    protected WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void Button_Demo_1_test() {
+        // TODO: Add test steps here
+        driver.get("https://testtrack.org");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
