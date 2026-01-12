1a7376009c4988af66560842233f9053218a410b
'Webrocket AI' <'webrocket-ai@pro.accompio.com'>
2026-01-12 03:34:41 +0000

    feat(test): integrate page object

--- tests/Button Demo - 1.java
+++ tests/Button Demo - 1.java
@@ -6,12 +6,12 @@ import org.testng.annotations.AfterTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Button_Demo_1 {
-    
+public class Button_Demo_-_1 {
+
     protected WebDriver driver = new ChromeDriver();
 
     @Test
-    public void Button_Demo_1_test() {
+    public void Button_Demo_-_1_test() {
         driver.get("https://testtrack.org");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
