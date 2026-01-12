eb535c42590df49799760202c8f381ea32b98b17
Nuno Alves <nuno.alves@aptoide.com>
2026-01-12 16:13:08 +0000

    feat(test): integrate page object

--- tests/Button-Demo-1.java
+++ tests/Button-Demo-1.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import pageobjects.testtrack_org_PO;
 import pageobjects.testtrack_org_buttondemo_PO;
+import tmpksgktlxj.pageobjects.testtrack_org_buttondemo_PO as TmpTesttrackOrgButtondemoPO; // Note: Java does not support alias imports; adjust package usage as needed
 
 public class Button_Demo_1 {
 
@@ -24,6 +25,9 @@ public class Button_Demo_1 {
 
         testtrack_org_buttondemo_PO testtrackOrgButtondemoPO = new testtrack_org_buttondemo_PO(driver);
         testtrackOrgButtondemoPO.clickPrimaryButton();
+
+        tmpksgktlxj.pageobjects.testtrack_org_buttondemo_PO secondaryButtonPO = new tmpksgktlxj.pageobjects.testtrack_org_buttondemo_PO(driver);
+        secondaryButtonPO.clickSecondaryButton();
     }
 
     @AfterTest
